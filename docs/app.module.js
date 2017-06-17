26d00669b5aa7e4b94984e409987a1ac2ffbd7d5
Evan Morrison <evanrmorrison@gmail.com>
2017-06-17 10:26:58 -0700

    add docs folder for gh-pages

---
+++ docs/app.module.js
@@ -0,0 +1,15 @@
+// Define the 'MTWApp' module
+
+(function() {
+
+ angular.module('MTWApp', [
+    'ngMaterial',
+    'ui.router',
+    'Navbar',
+    'Home',
+    'About',
+    'Services',
+    'Scheduling'
+  ])
+
+})();
