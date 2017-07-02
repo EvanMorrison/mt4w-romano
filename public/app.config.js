9c573416cb7c631ac2e0f3b3adee5dc65eee398e
Evan Morrison <evanrmorrison@gmail.com>
2017-07-02 11:39:25 -0700

    Ui route refactor (#3)
    * replace logo with new art
    
    * replace logo with new art
    
    * new logo image
    
    * Squashed commit of the following:
    
    commit c34eeafb4bbb8fbecc24e925035b8c6998d95df7
    Author: Evan Morrison <evanrmorrison@gmail.com>
    Date:   Sat Jun 17 21:39:54 2017 -0700
    
        reverting to earlier version
    
    commit a8c0f8724de50c2fcca29a36cf25a063e15eafe9
    Author: Evan Morrison <evanrmorrison@gmail.com>
    Date:   Sat Jun 17 10:53:43 2017 -0700
    
        clean up and remove docs subfolder
    
    commit c6be9d7b424bfde1c3d534d89dd13c574cc2e10a
    Merge: c978324 62be663
    Author: Evan Morrison <evanrmorrison@gmail.com>
    Date:   Sat Jun 17 10:52:13 2017 -0700
    
        Merge branch 'master' of https://github.com/EvanMorrison/mt4w-romano
    
    commit c97832463cfa6a1bb2dff995a1f2b5d28f0d47cf
    Author: Evan Morrison <evanrmorrison@gmail.com>
    Date:   Sat Jun 17 10:49:23 2017 -0700
    
        remove docs subfolder for gh-pages
    
    commit 62be6632b28b02178793f8d4f62c8d845cb17b52
    Author: Evan Morrison <evanrmorrison@gmail.com>
    Date:   Sat Jun 17 10:40:51 2017 -0700
    
        Create .nojekyll
    
    commit 26d00669b5aa7e4b94984e409987a1ac2ffbd7d5
    Author: Evan Morrison <evanrmorrison@gmail.com>
    Date:   Sat Jun 17 10:26:58 2017 -0700
    
        add docs folder for gh-pages
    
    commit dd156e2b8e768b1d4c58848a127dfe9752243680
    Merge: 7097717 8871d53
    Author: Evan Morrison <evanrmorrison@gmail.com>
    Date:   Wed Jun 14 18:18:03 2017 -0600
    
        Merge branch 'master' of https://git.heroku.com/manualtherapy4wellness
    
    commit 70977174b6a64202b72c4b0b98388aee71b74150
    Author: Evan Morrison <evanrmorrison@gmail.com>
    Date:   Wed Jun 14 18:07:00 2017 -0600
    
        add new logo
    
    commit 8871d5386754f5f3cbd3652d8b692127b1b1f6f3
    Author: Evan Morrison <evanrmorrison@gmail.com>
    Date:   Wed Jun 14 18:07:00 2017 -0600
    
        add new logo
    
    * refactor for named views and components, adaptive navigtaion

--- public/app.config.js
+++ public/app.config.js
@@ -2,29 +2,60 @@
 
   angular
     .module('MTWApp')
+
+    .config(function($mdThemingProvider){
+      $mdThemingProvider.theme('default')
+        .primaryPalette('indigo')
+      $mdThemingProvider.theme('alt')
+        .primaryPalette('light-green')
+        .accentPalette('purple')
+        .warnPalette('red')
+
+      
+      
+    })
+
     .config(['$locationProvider', '$stateProvider', '$urlRouterProvider',
       function config($locationProvider, $stateProvider, $urlRouterProvider) {
         $locationProvider.html5Mode(true);
 
         $urlRouterProvider.otherwise('/');
 
-
         $stateProvider
+          .state('main', {
+            url:'/',
+             views: {
+                'navbar': {component: 'navbar'},
+                'contentContainer': {component: 'home'}
+              }
+          })
           .state('home', {
-            url: '/',
-            component: 'home'
+            parent: 'main',
+            url: '',
+            views: {
+              'contentContainer@': {component: 'home'}
+            }
           })
           .state('about', {
-            url: '/about',
-            component: 'about'
+            parent: 'main',
+            url: 'about',
+            views: {
+              'contentContainer@': {component: 'about'}
+            }
           })
           .state('services', {
-            url: '/services',
-            component: 'services'
+            parent: 'main',
+            url: 'services',
+            views: { 
+              'contentContainer@': {component: 'services'}
+          }
           })
-          .state('scheduling', {
-            url: '/appointments',
-            component: 'scheduling'
+          .state('appointments', {
+            parent: 'main',
+            url: 'appointments',
+            views: {
+              'contentContainer@': {component: 'scheduling'}
+          }
           })
 
         
