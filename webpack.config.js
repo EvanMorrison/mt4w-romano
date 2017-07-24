97eb4aa698199af982e40bc4bd7a54e8af9a2375
Evan Morrison <evanrmorrison@gmail.com>
2017-07-23 18:41:10 -0600

    migrating to webpack 3, bundling images with loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,93 @@
 const path = require('path');
 
 const HTMLWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
 
+module.exports = (env = {}) => {
+  
+  const isProduction = env.production === true;
+  
+  return    {
 
-module.exports = {
+      entry: {
+        vendor: './src/vendor.js',
+        index: './src/app.module.js'
+      },
 
-  entry: {
-    vendor: './src/vendor.js',
-    index: './src/app.module.js'
-  },
+      output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].bundle.js'
+      },
 
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].bundle.js'
-  },
+      devtool: (() => {
+            if (isProduction) return 'hidden-source-map'
+            else return 'cheap-module-source-map'
+      })(),
 
-  devtool: 'cheap-module-source-map',
+      devServer: (() => {
+            if (isProduction) return {}
+            else return {
+                          contentBase: './dist',
+                          historyApiFallback: true
+                        }
+      })(), 
 
-  devServer: {
-    contentBase: './dist',
-    historyApiFallback: true
-  },
+      module: {
+        rules: [
+          { test: /\.js$/, exclude: /node_modules/, use: 'babel-loader' },
+          { test: /\.html$/, exclude: /node_modules/, use: 'raw-loader' },
+          { test: /\.(jpe?g|png|gif)$/, 
+            exclude: /node_modules/, 
+            use: [{
+                      loader: 'url-loader',
+                      options: {
+                          limit: 10000,
+                          name: 'assets/[hash].[ext]'
+                      }
+                  }]
+          },
+          { test: /\.css$/, 
+            use: (() => {
+                  if (isProduction) return ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: 'css-loader?sourceMap'
+                  });
+                  else return ['style-loader', 'css-loader']
+            })()
+          }   
+        ]
+      },
 
-  module: {
-    rules: [
-      { test: /\.js$/, exclude: /node_modules/, use: 'babel-loader' },
-      { test: /\.html$/, exclude: /node_modules/, use: 'raw-loader' },
-      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
-      { test: /\.(jpe?g|png|gif)$/, exclude: /node_modules/, 
-        use: 'file-loader?name=assets/[hash].[ext]'}
-    ]
-  },
+      plugins: (() => {
+        const pluginList = [
+          // plugins used by dev and production
+            new HTMLWebpackPlugin({
+                template: path.resolve(__dirname, 'src/index.html'),
+                filename: 'index.html',
+                inject: 'body'
+            })
+        ];
+          // plugins for production only
+          if (isProduction) {
+            pluginList.push(
+              new CleanWebpackPlugin(['dist']),
+              new ExtractTextPlugin({
+                filename: '[name].[contenthash].css'
+              }),
+              new CompressionPlugin({
+                asset: "[path].gz[query]",
+                algorithm: "gzip",
+                threshold: 10240,
+                test: /\.js$|\.html$|\.css$/,
+                minRatio: 0.8
+              })
+            )
+          }
+          return pluginList;
+      })()
 
-  plugins: [
-    new HTMLWebpackPlugin({
-        template: path.resolve(__dirname, 'src/index.html'),
-        filename: 'index.html',
-        inject: 'body'
-    })
-  ]
+  }
 
 }
